438f5628089a3645d41cecd496bc2b4b8d26ef31
TrajcheIvanov <79934583+TrajcheIvanov@users.noreply.github.com>
2021-04-17 09:46:40 +0200

    Initial commit

---
+++ TicketingSystem/TicketingSystem/Migrations/20210417074408_startingDatabaseWithUsersTable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TicketingSystem.Migrations
+{
+    public partial class startingDatabaseWithUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ TicketingSystem/TicketingSystem/Repository/TicketingSystemDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TicketingSystem.Models;
+
+namespace TicketingSystem.Repository
+{
+    public class TicketingSystemDbContext : DbContext
+    {
+        public TicketingSystemDbContext(DbContextOptions<TicketingSystemDbContext> options) : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
